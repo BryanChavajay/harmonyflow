fea85b438ab397f54d737d4126d968d3bff46992
Bryan Chavajay <bchavajaydev@gmail.com>
2024-11-08 23:27:55 -0600

    Precarga actualizada

--- core/db/02-Precarga.sql
+++ core/db/02-Precarga.sql
@@ -2,3 +2,10 @@ INSERT INTO modulos(modulo) VALUES ('MI-AGENDA'),('MI-TABLERO'),('BACKLOG'),('PR
 ('LINEAS-DESARROLLO'),('ESTADISTICAS'),('USUARIOS'),('ROLES');
 
 INSERT INTO roles(rol) VALUES('ADMIN'),('SR'),('JR');
+
+INSERT INTO estados(estado) VALUES ('EN PROGRESO'), ('EN CURSO'), ('TERMINADO')
+
+INSERT INTO lineas_desarrollo(linea) VALUES ('GENERAL')
+
+INSERT INTO proyectos(nombre, descripcion, id_linea, costo_estimado, fecha_inicio, fecha_final, activo, id_usuario_lider, eliminado)
+	VALUES ('SIN PROYECTO ASIGNADO', 'Destinado a registrar tareas generales', 1, 0, '2024-01-01', '2024-12-31', TRUE, 1, FALSE)
