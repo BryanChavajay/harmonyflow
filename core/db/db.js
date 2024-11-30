50d81c17d05e5c166a0b537a1752dfb445540aaa
Bryan Chavajay <bchavajaydev@gmail.com>
2024-11-29 20:50:05 -0600

    Conexion a la DB por ssh

--- core/db/db.js
+++ core/db/db.js
@@ -13,4 +13,13 @@ export const sequelize = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {
   port: DB_PORT,
   dialect: "postgres",
   logging: DB_PRODUCTION === "development" ? console.log : false,
+  dialectOptions:
+    DB_PRODUCTION === "production"
+      ? {
+          ssl: {
+            require: true,
+            rejectUnauthorized: false,
+          },
+        }
+      : {},
 });
